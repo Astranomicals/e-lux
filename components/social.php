1b2d1e67f539325f423a1e4d4fe6c0458cd7a913
Nick Gonzales <ngonzales07@gmail.com>
2023-06-08 18:19:28 -0700

    more inner page work done

---
+++ components/social.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Display social icons
+ *
+ * @category   Components
+ * @package    WordPress
+ * @subpackage Incredible Theme
+ * @author     Nick Gonzales
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://www.incrediblemarketing.com/
+ * @since      1.0.0
+ */
+
+$social_icon_class_map = array(
+	'facebook'   => 'fab fa-facebook-f',
+	'instagram'  => 'fab fa-instagram',
+	'pinterest'  => 'fab fa-pinterest-p',
+	'twitter'    => 'fab fa-twitter',
+	'linkedin'   => 'fab fa-linkedin-in',
+	'youtube'    => 'fab fa-youtube',
+	'vimeo'      => 'fab fa-vimeo-v',
+	'googleplus' => 'fab fa-google-plus-g',
+	'yelp'       => 'fab fa-yelp',
+	'call'       => 'fas fa-phone',
+	'email'      => 'fas fa-envelope',
+	'location'   => 'fas fa-map-marker-alt',
+);
+?>
+
+<?php if (have_rows('social_icons', 'option')) : ?>
+	<ul class="social--icons">
+		<?php
+		while (have_rows('social_icons', 'option')) :
+			the_row();
+		?>
+			<li><a href="<?php echo esc_attr(get_sub_field('social_url')); ?>" target="_blank">
+					<i class="<?php echo $social_icon_class_map[get_sub_field('social_icon')]; ?>"></i>
+				</a></li>
+		<?php endwhile; ?>
+	</ul>
+<?php endif; ?>
---
+++ page-contact.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Template Name: Contact Page
+ * Template Post Type: page
+ *
+ * @category   Components
+ * @package    WordPress
+ * @subpackage Incredible Theme
+ * @author     Nick Gonzales
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://www.incrediblemarketing.com/
+ * @since      1.0.0
+ */
+
+$address      = get_field('business_street_address', 'option');
+$address2     = get_field('business_city_state_zip', 'option');
+$address_link = get_field('business_address_link', 'option');
+$phone        = get_field('business_phone_display', 'option');
+$phone_url    = get_field('business_phone_url', 'option');
+$hours    		= get_field('business_hours', 'option');
+get_header();  ?>
+
+<section class="block block--top-header">
+	<div class="side--image">
+		<?php echo get_the_post_thumbnail($post->ID, 'top_header_thumb'); ?>
+	</div>
+	<div class="container">
+		<div class="col-xl-6">
+			<?php echo do_shortcode('[gravityforms id="3" title="false" description="false" ajax="true"]'); ?>
+		</div>
+	</div>
+</section>
+
+<section class="block block--content">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="content--info">
+					<div class="content">
+						<?php the_content(); ?>
+					</div>
+					<div class="info">
+						<h4>Phone Number</h4>
+						<p><a href="tel:<?php echo $phone_url; ?>"><?php echo $phone; ?></a></p>
+						<h4>Directions</h4>
+						<p><a href="<?php echo $address_link; ?>" target="_blank"><?php echo $address; ?><br /><?php echo $address2; ?></a></p>
+						<h4>Hours</h4>
+						<?php echo $hours; ?>
+						<?php get_template_part('components/social'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
